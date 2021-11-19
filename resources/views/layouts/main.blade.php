93801961484338e454f6f60a5a35982e3ebd27ba
Roman Gurniy <roman.gurniy@monogo.pl>
2021-11-20 00:28:03 +0200

    Install tailwindcss

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Main</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="pop-up-message">
+    <div class="pop-up-message-image">
+        <img class="h-12 w-12" src="{{asset('img/logo.svg') }}" alt="ChitChat Logo">
+    </div>
+    <div>
+        <div class="pop-up-message-title">ChitChat</div>
+        <div class="pop-up-message-text">You have a new message!</div>
+    </div>
+</div>
+
+</body>
+</html>
